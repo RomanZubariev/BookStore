618d941a0c00c21b8c826375ad2dcc9d77cd3237
RomanZubariev <rominovladenie@yandex.com>
2017-01-31 19:37:42 +0200

    Initial commit

---
+++ src/main/java/DBUtil.java
@@ -0,0 +1,160 @@
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+
+
+public class DBUtil {
+    private Connection connection = null;
+    public DBUtil() {
+
+
+    }
+    public void createConnection(String hostName, String dbName, String login, String password){
+        try {
+            String connectionData = "jdbc:postgresql://"+hostName+ dbName;
+            Class.forName("org.postgresql.Driver");
+
+            connection = DriverManager.getConnection(
+                    connectionData, login, password);
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        if (connection!=null) System.out.println("Connected");
+                else System.out.println("Still Null");
+    }
+
+    public void executeStatement(String statementStr){
+        if (connection!=null){
+
+            try (Statement statement = connection.createStatement()) {
+                statement.execute(statementStr);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        } else System.out.println("No connection to DB");
+    }
+    public void createSchema(String schema){
+        System.out.println("Schema "+schema+" to be created...");
+        executeStatement("CREATE SCHEMA IF NOT EXISTS "+schema);
+    }
+    public void setSchema(String schema){
+
+        try (Statement statement = connection.createStatement()){
+            statement.execute("set search_path to " + schema +";");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public JSONArray readAllData(String schemaName, String tableName){
+        JSONArray jsonArray = new JSONArray();
+        try (Statement statement = connection.createStatement();
+             ResultSet resultSet = connection.createStatement().executeQuery("Select*From "+tableName+";")){
+            statement.execute("set search_path to " + schemaName + ";");
+            ResultSetMetaData resMD = resultSet.getMetaData();
+            while (resultSet.next()){
+                int numColumns = resMD.getColumnCount();
+                JSONObject jsonObject = new JSONObject();
+                for (int i=1;i<numColumns+1;i++){
+                    String columnName = resMD.getColumnName(i);
+                    if (resMD.getColumnType(i)== Types.VARCHAR)
+                        jsonObject.put(columnName,resultSet.getString(columnName));
+
+                    if(resMD.getColumnType(i)==Types.INTEGER)
+                        jsonObject.put(columnName,resultSet.getInt(columnName));
+                }
+                jsonArray.add(jsonObject);
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return jsonArray;
+    }
+    public void populateTable(ArrayList<HashMap<String, Object>> dataList, String tableName){
+        //populate table
+        //read column names from 1st row
+        HashMap<String,Object> firstRow = dataList.get(0);
+        StringBuilder columnsViaComa = new StringBuilder();
+        StringBuilder questionSignsViaComa=new StringBuilder();
+        Iterator iterator = firstRow.keySet().iterator();
+        while(iterator.hasNext()){
+            columnsViaComa.append(iterator.next());
+            questionSignsViaComa.append('?');
+            if (iterator.hasNext()){
+                columnsViaComa.append(',');
+                questionSignsViaComa.append(',');
+            }
+        }
+        String statement = "INSERT INTO "+ tableName+ " ( "+
+                columnsViaComa.toString()+") VALUES (" + questionSignsViaComa.toString()+");";
+
+        try (PreparedStatement prep = connection.prepareStatement(statement)){
+            for (int i = 0; i<dataList.size(); i++){
+
+                int j=0;
+                for (String key: dataList.get(i).keySet()){
+                    j++;
+                    prep.setObject(j,dataList.get(i).get(key));
+                }
+                prep.execute();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public XSSFWorkbook writeTableToExcel(String tableName){
+        XSSFWorkbook workbook = new XSSFWorkbook();
+        try (Statement statement= connection.createStatement();
+             ResultSet resultSet= statement.executeQuery("SELECT*FROM "+tableName+";")) {
+            ResultSetMetaData rsMD = resultSet.getMetaData();
+            XSSFSheet sheet = workbook.createSheet(tableName);
+            //create row with column titles
+            XSSFRow row = sheet.createRow(1);
+            XSSFCell cell;
+            for (int counter=1; counter<rsMD.getColumnCount()+1;counter++){
+                cell = row.createCell(counter);
+                cell.setCellValue(rsMD.getColumnName(counter));
+            }
+            int j=2;//first row occupied with column titles
+            while(resultSet.next()){
+                row = sheet.createRow(j);
+                for (int i=1; i<=rsMD.getColumnCount();i++){
+                    cell = row.createCell(i);
+                    if (rsMD.getColumnType(i)==Types.INTEGER)
+                        cell.setCellValue(resultSet.getInt(rsMD.getColumnName(i)));
+                    if (rsMD.getColumnType(i)==Types.VARCHAR)
+                        cell.setCellValue(resultSet.getString(rsMD.getColumnName(i)));
+                }
+                j++;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return workbook;
+    }
+    public Connection getConnection(){
+        return connection;
+    }
+    public void dropSchema(String schema){
+        System.out.println("Schema "+schema+" to be droped...");
+        executeStatement("DROP SCHEMA "+schema+" RESTRICT");
+    }
+    public void closeConnection(){
+        try {
+            System.out.println("Connection closed");
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/test/java/TestMain.java
@@ -0,0 +1,106 @@
+import org.json.simple.JSONArray;
+import org.junit.*;
+import java.io.*;
+import java.util.*;
+
+public class TestMain {
+    private final static String TABLE_NAME = "BOOKS";
+    private final static String SCHEMA_NAME = "BookSchema";
+    private static DBUtil dbUtil = new DBUtil();
+    @Before
+    public void createConnection(){
+        dbUtil.createConnection("localhost:5432/", "BookStoreDB", "postgres", "postgres");
+        dbUtil.createSchema(SCHEMA_NAME);
+        dbUtil.setSchema(SCHEMA_NAME);
+    }
+    @After
+    public void closeConnection(){
+        dbUtil.closeConnection();
+    }
+    @Test
+    public void createTable(){//create table if it does not exist
+        dbUtil.executeStatement("CREATE TABLE IF NOT EXISTS "+TABLE_NAME+"("+
+                "ID SERIAL  PRIMARY KEY,"+
+                TableFields.getFieldString(TableFields.Author)+" TEXT,"+
+                TableFields.getFieldString(TableFields.Name)+" TEXT,"+
+                TableFields.getFieldString(TableFields.Year)+" INT,"+
+                TableFields.getFieldString(TableFields.Text)+" TEXT);");
+    }
+    @Test
+    public void populateTable(){
+        int numOfRows = 1000;
+        ArrayList<HashMap<String, Object>> dataList = new ArrayList<HashMap<String, Object>>();
+        //fill array
+        for (int i = 0; i<numOfRows; i++){
+            HashMap<String, Object> map = new HashMap<String, Object>();
+            for (TableFields field:TableFields.values())
+                if (field == TableFields.Year)
+                    map.put(TableFields.getFieldString(field), generateYear());
+                else map.put(TableFields.getFieldString(field), generateName());
+            //Here all the fields except Year are set as a single "word"
+            dataList.add(map);
+        }
+        dbUtil.populateTable(dataList, TABLE_NAME);
+    }
+    @Test
+    public void  jsonTest(){
+        JSONArray jArray = dbUtil.readAllData(SCHEMA_NAME,TABLE_NAME);
+        try {
+            FileWriter file = new FileWriter("JSON_FILE.txt");
+            file.write(jArray.toJSONString());
+            System.out.println("JSON file created");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @Test
+    public void copyDBtoExcel(){
+        try {
+            FileOutputStream outStream = new FileOutputStream(new File("excel_FILE.xlsx"));
+            dbUtil.writeTableToExcel(TABLE_NAME).write(outStream);
+            outStream.close();
+            System.out.println("EXCEL file was written");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static int generateYear(){
+        Random random = new Random();
+        return 1900+random.nextInt(116);
+    }
+    private static String generateName(){
+        Random random = new Random();
+        char[] word = new char[5+random.nextInt(5)];
+        for(int counter=0;counter<word.length;counter++){
+            word[counter]=(char)('a'+random.nextInt(26));
+        }
+        return new String(word);
+    }
+
+    private enum TableFields{
+        Author, Name, Year, Text;
+        public static String getFieldString(TableFields field){
+            String fieldString ="";
+            switch (field){
+                case Author:
+                    fieldString = "Author";
+                    break;
+                case Name:
+                    fieldString = "Name";
+                    break;
+                case Year:
+                    fieldString = "Year";
+                    break;
+                case Text:
+                    fieldString ="Text";
+                    break;
+                default:
+                    fieldString="";
+                    break;
+            }
+            return fieldString;
+        }
+    }
+}
